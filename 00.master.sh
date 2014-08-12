32daa84e4c392b2956b1139b4aae806a0cc08867
Cameron Prybol <cprybol1@uga.edu>
2014-08-12 10:16:32 -0400

    initial commit of files

---
+++ 00.master.sh
@@ -0,0 +1,18 @@
+#bin/bash
+
+# Author: Cameron Prybol
+# Created: 2014.06.17
+# Last Updated: 
+# Description: 
+
+#BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | perl -pe 's|/[A-Z_]+/[A-Z_]+$||g')"
+BASE="/lustre1/escratch1/cprybol1_Jul_28"
+FILES="$BASE/ESSENTIAL/SCRIPTS"
+
+"$FILES/01.map_to_OR.sh" & "$FILES/02.map_to_MV.sh" & wait
+"$FILES/03.remove_MV_snps.sh"
+"$FILES/04.mpileup.sh"
+"$FILES/05.gff_filtered_bam.sh"
+"$FILES/06.filter_sam_by_kb.sh"
+"$FILES/07.bcf_vcf"
+
---
+++ 01.map_to_OR.sh
@@ -0,0 +1,49 @@
+#bin/bash
+
+# Author: Cameron Prybol
+# Created: 2014.06.17
+# Last Updated: 2014.06.19
+# Description: maps fastQ sequence files of N. crassa to the oak ridge genome
+
+#BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd | perl -pe 's|/[A-Z_]+/[A-Z_]+$||g')"
+BASE="/lustre1/escratch1/cprybol1_Jul_30"
+FILES="$BASE"/ESSENTIAL/MERGED_FASTQ/*
+
+##############################################################
+#	build index from reference genome
+############################################################
+
+/usr/local/bwa/latest/bwa index "$BASE/ESSENTIAL/REF_GENOMES/Ncrassa_OakRidge/neurospora_crassa_or74a_12_supercontigs.fasta"
+
+
+###############################################################
+#	if bam output folder doesn't exist, make it
+###############################################################
+
+if [ ! -d "$BASE/OR_MAP_BAM" ];
+        then
+                mkdir "$BASE/OR_MAP_BAM"
+                echo "> created directory $BASE/OR_MAP_BAM"
+fi
+
+##############################################################
+#	map fasta files to bowtie reference genome files
+##############################################################
+
+# determine number of files, used for creating readable output to user
+num_files=$(ls -1 "$BASE"/ESSENTIAL/MERGED_FASTQ/ | wc -l)
+
+i=1
+
+BAM_DIR="$BASE/OR_MAP_BAM"
+
+for f in $FILES
+do
+
+	# /usr/local/bwa/latest/bwa mem [options] <idxbase> <in1.fq> [in2.fq]
+	file=${f##*/}
+	file_name=$(echo "$file" | sed -e 's/\.f\(q\|astq\)//')
+	echo "> Processing file $i of $num_files : $file"
+	/usr/local/bwa/latest/bwa mem "$BASE/ESSENTIAL/REF_GENOMES/Ncrassa_OakRidge/neurospora_crassa_or74a_12_supercontigs.fasta" "$f" | samtools view -buS - | samtools sort - "$BAM_DIR/$file_name.sorted"
+	let i=i+1
+done
