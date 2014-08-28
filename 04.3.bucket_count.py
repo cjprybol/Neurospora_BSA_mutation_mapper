26059368d6b552ae8611304e99230a6b9b667289
Cameron Prybol <cprybol1@uga.edu>
2014-08-28 16:26:34 -0400

    updated 04.3 to work with sliding frames 1/5 the size of total window size. should make that user definable as a command line option next

--- 04.3.bucket_count.py
+++ 04.3.bucket_count.py
@@ -9,6 +9,7 @@ import sys
 
 
 count = 5000	# size of buckets
+slide_size = count // 5
 in_file = ''
 out_file = ''
 if (len(sys.argv) == 2):
@@ -21,44 +22,48 @@ out_file = in_file + '_' + str(count) + 'bp_count'
 
 counts = []
 
-current_bucket = 0
-current_total = 0
-records_read = 0
-records_skipped = 0
-with open(in_file) as f:
-	for line in f:
-		line = line.strip("\n").split()
-		temp = int(line[1])-1
-		bucket = temp//count
-		if (bucket > current_bucket):
-			if (records_read > 0):
-				counts.append([(current_bucket * count) + 1,current_total/records_read,records_read,records_skipped])
+for i in range(0,5):
+	current_bucket = 0
+	offset = i * slide_size 
+	current_total = 0
+	records_read = 0
+	records_skipped = 0
+	with open(in_file) as f:
+		for q in range(0,offset):
+			next(f)
+		for line in f:
+			line = line.strip("\n").split()
+			temp = int(line[1])-1
+			bucket = (temp-offset)//count
+			if (bucket > current_bucket):
+				if (records_read > 0):
+					counts.append([(current_bucket * count) + 1 + offset,current_total/records_read,records_read,records_skipped])
+				else:
+					counts.append([(current_bucket * count) + 1 + offset,'0',records_read,records_skipped])
+				current_bucket = bucket
+				current_total = 0
+				records_read = 0
+				records_skipped = 0
+			if (int(line[2]) != 0 and int(line[3]) != 0 and float(line[4]) != 0):	#don't pull down average by including positions without any reads
+				current_total += float(line[4])
+				records_read += 1
 			else:
-				counts.append([(current_bucket * count) + 1,'0',records_read,records_skipped])
-			
-			#####################################
-#			print('read', records_read, 'skipped', records_skipped, sep='\t')
-			#####################################
+				records_skipped += 1
+	if (records_read > 0):
+		counts.append([(current_bucket * count) + 1 + offset,current_total/records_read,records_read,records_skipped])
+	else:
+		counts.append([(current_bucket * count) + 1 + offset,'0',records_read,records_skipped])
 
-			current_bucket = bucket
-			current_total = 0
-			records_read = 0
-			records_skipped = 0
-		if (int(line[2]) != 0 and int(line[3]) != 0 and float(line[4]) != 0):	#don't pull down average by including positions without any reads
-			current_total += float(line[4])
-			records_read += 1
-		else:
-			records_skipped += 1
-if (records_read > 0):
-	counts.append([(current_bucket * count) + 1,current_total/records_read,records_read,records_skipped])
-else:
-	counts.append([(current_bucket * count) + 1,'0',records_read,records_skipped])
+counts.sort()
+for j in counts:
+	print(j)
 
-out = open(out_file,'w')
-
-
-for value in counts:
-	output = '\t'.join(map(str, value)) + '\n'
-	out.write(output)
-
-out.close()
+#	out = open(out_file,'w')
+#	
+#	
+#	for value in counts:
+#		output = '\t'.join(map(str, value)) + '\n'
+#		print(output.strip())
+#		out.write(output)
+#	
+#	out.close()
