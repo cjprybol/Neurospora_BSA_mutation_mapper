bd5f75b5119c776af575a28e403ed1b0db646d29
Cameron Prybol <cprybol1@uga.edu>
2015-01-05 17:10:03 -0500

    Ready to run up until 025

---
+++ SCRIPTS/025.parse_snps.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# 
+
+BASE="/escratch4/cprybol1/cprybol1_Nov_19"
+FILES="$BASE"/VCF_OUTPUT/*
+
+for f in $FILES
+do
+
+        # create variable with filename without full directory path
+        in_file=${f##*/}
+
+	# create variable with output filename where input bam file extension is swapped for output vcf file extension
+        out_vcf=$(echo "$in_file" | sed -e 's/\.bam/\.vcf/')
+
+	### figure out how to run python script from here
+
+done
