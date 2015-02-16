26a4df6d37cef1535e50618737c02b9b348216f2
Cameron Prybol <cprybol1@uga.edu>
2015-02-16 15:03:03 -0500

    done!! it all works!!

--- SCRIPTS/045.2.translate.py
+++ SCRIPTS/045.2.translate.py
@@ -3,6 +3,8 @@
 import sys
 import subprocess
 import pandas as pd
+from Bio.Seq import Seq
+import difflib
 
 SNP_GFF = sys.argv[1]
 FASTA_SEQ = sys.argv[2]
@@ -26,4 +28,34 @@ seq_data.index.names = ['name']
 
 merged = pd.merge(snp_data, seq_data, left_index=True, right_index=True)
 
-print(merged)
+f = open(OUTFILE, 'w')
+
+for i in merged.index:
+	position = merged.at[i,'pos'] - merged.at[i,'start']
+	snp_len = len(merged.at[i,'ref'])
+	offset = merged.at[i,'offset']
+	base = ""
+	seq = ""
+	if (merged.at[i,'strand'] == "+"):
+		seq = Seq(merged.at[i,'sequence'])
+	elif (merged.at[i,'strand'] == "-"):
+		seq = Seq(merged.at[i,'sequence']).reverse_complement()
+	new_seq = seq[:position] + merged.at[i,'alt'] + seq[position+snp_len:]
+	if (merged.at[i,'strand'] == "-"):
+		seq = seq.reverse_complement()
+		new_seq = new_seq.reverse_complement()
+	ref_tr_seq = str(seq.translate())[offset:]
+	new_tr_seq = str(new_seq.translate())[offset:]
+
+	# split every n  nucleotides
+	n = 50
+	ref_list = [(str(i+1) + "." + ref_tr_seq[i:i+n]) for i in range(0, len(ref_tr_seq), n)]
+	new_list = [(str(i+1) + "." + new_tr_seq[i:i+n]) for i in range(0, len(new_tr_seq), n)]
+
+	diff = difflib.Differ().compare(ref_list, new_list)
+	expected_AA = (position//3) + 1
+	print('Transcript', 'Pos', 'Ref', 'Alt', 'AA Location', sep="\t\t", file=f)
+	print(i, merged.at[i,'pos'], merged.at[i,'ref'], merged.at[i,'alt'], expected_AA, sep="\t\t", file=f)
+	print(file=f)
+	print('sequence:', "\n".join(diff), sep="\n", file=f)
+	print(file=f)
--- SCRIPTS/045.translate_cds.sh
+++ SCRIPTS/045.translate_cds.sh
@@ -26,13 +26,13 @@ do
 	in_file="${f##*/}"
 	file_head=$( echo "$in_file" | perl -pe 's/\.CDS//')
 
-#	perl -pe 's/.*?Parent=(NCU[0-9]+T[0-9]).*/$1/' $f > "$OUT_DIR/$file_head.transcripts.temp"
+	perl -pe 's/.*?Parent=(NCU[0-9]+T[0-9]).*/$1/' $f > "$OUT_DIR/$file_head.transcripts.temp"
 
-#	python3 045.1.extract_fasta_seq.py "$OUT_DIR/$file_head.transcripts.temp" "$TRANSCRIPTS" "$OUT_DIR/$file_head.fasta_extract"
+	python3 045.1.extract_fasta_seq.py "$OUT_DIR/$file_head.transcripts.temp" "$TRANSCRIPTS" "$OUT_DIR/$file_head.fasta_extract"
 	python3 045.2.translate.py "$f" "$OUT_DIR/$file_head.fasta_extract" "$OUT_DIR/$file_head.translated_CDS"
 
-#	rm "$OUT_DIR/$file_head.transcripts.temp"
-#	rm "$OUT_DIR/$file_head.fasta_extract"
+	rm "$OUT_DIR/$file_head.transcripts.temp"
+	rm "$OUT_DIR/$file_head.fasta_extract"
 
 
 done
