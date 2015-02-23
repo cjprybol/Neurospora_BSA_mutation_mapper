a08c16c6ea5039b6faf6ec4be27e419493947052
Cameron Prybol <cprybol1@uga.edu>
2015-02-23 16:57:52 +0000

    updated script names, which were incorrect before

--- SCRIPTS/020.compare_data_to_genomes.sh
+++ SCRIPTS/020.compare_data_to_genomes.sh
@@ -10,7 +10,6 @@ FILES="$BASE/ESSENTIAL/SCRIPTS"
 
 #sh $FILES/021.single_end_map_to_OR.sh
 #sh $FILES/021.paired_end_map_to_OR.sh
-sh $FILES/022.simulate_MV_reads.sh
-sh $FILES/023.map_sim_reads_to_OR.sh
+sh $FILES/022.bed_filtered_bam.sh
 sh $FILES/024.find_snps_between_genomes.sh
-sh $FILES/025.parse_snps.py
+sh $FILES/025.parse_snps.sh
