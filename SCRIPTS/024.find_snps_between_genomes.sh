369742e3508c26d25c8bfc4edc0993bfdb347a14
Cameron Prybol <cprybol1@uga.edu>
2015-02-26 17:38:52 +0000

    removed excess commenting

--- SCRIPTS/024.find_snps_between_genomes.sh
+++ SCRIPTS/024.find_snps_between_genomes.sh
@@ -28,14 +28,14 @@ do
 	# create variable with output filename where input bam file extension is swapped for output vcf file extension
         out_vcf=$(echo "$in_file" | sed -e 's/\.bam/\.vcf/')
 
-#		#	mpilup options
-#		#	-u	Generate uncompressed VCF/BCF output, which is preferred for piping
-#		#	-g	Compute genotype likelihoods and output them in the binary call format (BCF). As of v1.0, this is BCF2 which is incompatible with the BCF1 format produced by previous (0.1.x) versions of samtools.
-#		#	-f	reference fasta	
-#		#	bcftools call options
-#		#	−O v	output vcf format
-#		#	-o	output filename
-#	
+	#	mpilup options
+	#	-u	Generate uncompressed VCF/BCF output, which is preferred for piping
+	#	-g	Compute genotype likelihoods and output them in the binary call format (BCF). As of v1.0, this is BCF2 which is incompatible with the BCF1 format produced by previous (0.1.x) versions of samtools.
+	#	-f	reference fasta	
+	#	bcftools call options
+	#	−O v	output vcf format
+	#	-o	output filename
+
 	samtools mpileup -ugf "$REF_GENOME_DIR/neurospora_crassa_or74a_12_supercontigs.fasta" "$IN_DIR/$in_file" | bcftools call -vmO z -o "$OUT_VCF_DIR/$out_vcf.gz"
 	tabix -p vcf "$OUT_VCF_DIR/$out_vcf.gz"
 	bcftools stats -F "$REF_GENOME_DIR/neurospora_crassa_or74a_12_supercontigs.fasta" -s - "$OUT_VCF_DIR/$out_vcf.gz" > "$OUT_VCF_DIR/$out_vcf.gz.stats"
