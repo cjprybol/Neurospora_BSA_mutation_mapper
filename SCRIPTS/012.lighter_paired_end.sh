43afc83d7dc5995239190413f64cf8ade88940a0
Cameron Prybol <cprybol1@uga.edu>
2015-02-28 12:40:17 -0500

    updated to handle unexpected file output name

--- SCRIPTS/012.lighter_paired_end.sh
+++ SCRIPTS/012.lighter_paired_end.sh
@@ -4,7 +4,7 @@
 cd `pwd`
 BASE="$( dirname "$( dirname "$( echo `pwd` )" )" )"
 
-FILES="$(ls "$BASE"/ESSENTIAL/FASTQ/*R1.fastq.gz)"
+FILES="$(ls "$BASE"/ESSENTIAL/FASTQ/*.R1.fastq.gz)"
 
 OUT_DIR="$BASE/LIGHTER_FASTQ"
 
@@ -19,15 +19,16 @@ fi
 # write blank file to append output data to
 > "$OUT_DIR/lighter.out"
 
+echo "$FILES"
+
 # run ligther on all fastq files
 for f in $FILES
 do
 
-	
-	forward_head=${f##*/}
-	reverse="$( echo $f | perl -pe 's/\.R1\.fastq\.gz/\.R2\.fastq\.gz/' )"
-	reverse_head=${reverse##*/}
-	echo -e "running Lighter on \n\t $forward_head \t $reverse_head" >> "$OUT_DIR/lighter.out"
+	forward_file=${f##*/}
+	reverse="$( echo $f | perl -pe 's/\.R1\.fastq/\.R2\.fastq/' )"
+	reverse_file=${reverse##*/}
+	echo -e "running Lighter on \n\t $forward_file \t $reverse_file" >> "$OUT_DIR/lighter.out"
 
 	# run lighter using the forward and reverse read pairs, with a k-mer size
 	# of 19 (see Figure 5 of paper)
@@ -37,5 +38,5 @@ do
 	# -stable: sequentialize the sampling stage, output the same result with different runs (default: false)
 	# -t num_of_threads: number of threads to use (default: 1)
 	lighter -r "$f" -r "$reverse" -K 19 41020000 -od "$OUT_DIR" -trim -stable -t 8 2>> "$OUT_DIR/lighter.out"
-	
+
 done
